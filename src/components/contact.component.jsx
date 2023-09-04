ea2e8b8ffd4dc597ee5b34b91f6dc4297a815888
Foluke Odusanya <odusanya.folukemi@gmail.com>
2023-09-04 09:00:41 +0100

    update resume and ontact section

--- src/components/contact.component.jsx
+++ src/components/contact.component.jsx
@@ -10,51 +10,28 @@ const Contact = () => {
   return (
     <section className="bg-lightDark h-screen pt-24 px-24 sm:px-2" id="contact">
       <Reveal effect="fadeInUp">
-        <h2 className="text-primary text-5xl text-center py-8 sm:text-3xl">CONTACT</h2>
-
-        <form className="px-24 py-8 grid gap-y-8 sm:px-2" onSubmit={handleSubmit}>
-          <input
-            placeholder="Your Name"
-            id="name"
-            type="text"
-            name="name"
-            className="h-14 rounded-md p-4 w-1/2 mx-auto sm:w-full"
-          />
-          <ValidationError prefix="Name" field="name" errors={state.errors} />
-          <input
-            placeholder="Your Email"
-            id="email"
-            type="email"
-            name="email"
-            className="h-14 rounded-md p-4 w-1/2 mx-auto sm:w-full"
-          />
-          <ValidationError prefix="Email" field="email" errors={state.errors} />
-          <textarea
-            placeholder="Your Message"
-            id="message"
-            name="message"
-            className="h-60 rounded-md p-4 w-1/2 mx-auto sm:w-full"
-          />
-          <ValidationError
-            prefix="Message"
-            field="message"
-            errors={state.errors}
-          />
-          {state.succeeded ? (
-            <p className="text-success w-1/2 mx-auto sm:w-full">
-              Message Sent Successfully!
-            </p>
-          ) : (
-            ""
-          )}
-          <button
-            className="w-60 mx-auto bg-primary border-primary border-2 rounded-full text-black px-10 py-4 text-xl cursor-pointer uppercase hover:opacity-60 transition-all"
-            disabled={state.submitting}
-            type="submit"
-          >
-            SEND MESSAGE
-          </button>
-        </form>
+        <h2 className="text-primary text-5xl text-center py-8 sm:text-3xl">
+          CONTACT
+        </h2>
+        <div className="space-y-10 my-16">
+          <h2 className="text-3xl">
+            Do you have an interesting project that requires my services? Please
+            contact me and let&apos;s have a chat about what I can do for you.
+          </h2>
+          <h2 className="text-3xl">
+            You can reach me out via email{" "}
+            <span className="text-primary font-medium">
+              <a href="mailto:odusanya.folukemi@gmail.com">
+                odusanya.folukemi@gmail.com
+              </a>
+            </span>
+            &nbsp; or a phone call
+            <span className="text-primary font-medium">
+              <a href="tel:+2348132693859"> +234 813 269 3859</a>
+            </span>{" "}
+          </h2>
+          <p className="text-3xl">Looking forward to hearing from you!</p>
+        </div>
       </Reveal>
     </section>
   );
